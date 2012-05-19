aa258064a343391136319e73bbfc2996663ab2db
Christoph Redl <redl@kr.tuwien.ac.at>
2012-05-19 13:50:47 +0000

    update shortest path algorithm

--- maze/trunk/src/maze.cpp
+++ maze/trunk/src/maze.cpp
@@ -12,11 +12,16 @@
 #include <dlvhex2/Registry.h>
 #include <dlvhex2/ProgramCtx.h>
 
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
 #include <string>
 #include <sstream>
 #include <fstream>
 #include <cstdio>
 
+using namespace boost;
 
 namespace dlvhex {
 namespace benchmark {
@@ -54,29 +59,29 @@ private:
 				if (!reachable[x][y - 1]){
 					dfstree[x][y] |= UP_MASK;
 					dfstree[x][y - 1] |= DOWN_MASK;
+					dfs_r(x, y - 1);
 				}
-				dfs_r(x, y - 1);
 			}
 			if (y < sizeY - 1 && maze[x][y] & DOWN_MASK && maze[x][y + 1] & UP_MASK){
 				if (!reachable[x][y + 1]){
 					dfstree[x][y] |= DOWN_MASK;
 					dfstree[x][y + 1] |= UP_MASK;
+					dfs_r(x, y + 1);
 				}
-				dfs_r(x, y + 1);
 			}
 			if (x > 0 && maze[x][y] & LEFT_MASK && maze[x - 1][y] & RIGHT_MASK){
 				if (!reachable[x - 1][y]){
 					dfstree[x][y] |= LEFT_MASK;
 					dfstree[x - 1][y] |= RIGHT_MASK;
+					dfs_r(x - 1, y);
 				}
-				dfs_r(x - 1, y);
 			}
 			if (x < sizeX - 1 && maze[x][y] & RIGHT_MASK && maze[x + 1][y] & LEFT_MASK){
 				if (!reachable[x + 1][y]){
 					dfstree[x][y] |= RIGHT_MASK;
 					dfstree[x + 1][y] |= LEFT_MASK;
+					dfs_r(x + 1, y);
 				}
-				dfs_r(x + 1, y);
 			}
 		}
 
@@ -172,6 +177,79 @@ private:
 
 			// run
 			dfs_r(x - 1, y - 1);
+		}
+
+		void findpath(int startX, int startY, int destX, int destY){
+			assert (startX >= 1 && startX <= sizeX);
+			assert (startY >= 1 && startY <= sizeY);
+
+			// reset
+			for (int xi = 0; xi < sizeX; ++xi){
+				for (int yi = 0; yi < sizeY; ++yi){
+					dfstree[xi][yi] = 0;
+				}
+			}
+
+			typedef adjacency_list < listS, vecS, undirectedS, no_property, property < edge_weight_t, int > > graph_t;
+			typedef graph_traits < graph_t >::vertex_descriptor vertex_descriptor;
+			typedef graph_traits < graph_t >::edge_descriptor edge_descriptor;
+
+			std::vector<std::pair<int, int> > edges;
+			for (int x = 0; x < sizeX; ++x){
+				for (int y = 0; y < sizeY; ++y){
+					if (x < sizeX - 1 && maze[x][y] & RIGHT_MASK && maze[x + 1][y] & LEFT_MASK){
+						edges.push_back(std::pair<int, int>(y * sizeX + x, y * sizeX + (x + 1)));
+					}
+					if (y < sizeY - 1 && maze[x][y] & DOWN_MASK && maze[x][y + 1] & UP_MASK){
+						edges.push_back(std::pair<int, int>(y * sizeX + x, (y + 1) * sizeX + x));
+					}
+				}
+			}
+			std::pair<int, int>* edgeArray = new std::pair<int, int>[edges.size()];
+			int* weights = new int[edges.size()];
+			for (int i = 0; i < edges.size(); ++i){
+				edgeArray[i] = edges[i];
+				weights[i] = 1;
+			}
+
+			graph_t g(edgeArray, edgeArray + edges.size(), weights, sizeX * sizeY);
+			property_map<graph_t, edge_weight_t>::type weightmap = get(edge_weight, g);
+
+			std::vector<vertex_descriptor> p(num_vertices(g));
+			std::vector<int> d(num_vertices(g));
+			vertex_descriptor s = vertex((startY - 1) * sizeX + (startX - 1), g);
+
+			dijkstra_shortest_paths(g, s, predecessor_map(&p[0]).distance_map(&d[0]));
+
+			int nx = destX - 1;
+			int ny = destY - 1;
+			while (nx != (startX - 1) || ny != (startY - 1)){
+				int px = p[ny * sizeX + nx] % sizeX;
+				int py = p[ny * sizeX + nx] / sizeX;
+
+				if (px < nx){
+					dfstree[nx][ny] |= LEFT_MASK;
+					dfstree[px][py] |= RIGHT_MASK;
+				}
+				if (px > nx){
+					dfstree[nx][ny] |= RIGHT_MASK;
+					dfstree[px][py] |= LEFT_MASK;
+				}
+				if (py < ny){
+					dfstree[nx][ny] |= UP_MASK;
+					dfstree[px][py] |= DOWN_MASK;
+				}
+				if (py > ny){
+					dfstree[nx][ny] |= DOWN_MASK;
+					dfstree[px][py] |= UP_MASK;
+				}
+
+				nx = px;
+				ny = py;
+			}
+
+			delete []edgeArray;
+			delete []weights;
 		}
 
 		int getWidth(){
@@ -313,7 +391,62 @@ public:
 		assert(false);
 	}
 
-//	virtual void generalizeNogood(Nogood ng, const std::set<ID>& auxes, ProgramCtx* ctx, NogoodContainerPtr nogoods){}
+	virtual void generalizeNogood(Nogood ng, const std::set<ID>& auxes, ProgramCtx* ctx, NogoodContainerPtr nogoods){
+
+		DBGLOG(DBG, "ReachableAtom::generalizeNogood");
+
+		// find the auxiliary in the nogood
+		ID patternID = ID_FAIL;
+		BOOST_FOREACH (ID l, ng){
+			const OrdinaryAtom& lAtom = ctx->registry()->ogatoms.getByAddress(l.address);
+
+			if (ctx->registry()->ogatoms.getIDByAddress(l.address).isExternalAuxiliary() && ctx->registry()->getIDByAuxiliaryConstantSymbol(lAtom.tuple[0]) == getPredicateID()){
+				patternID = l;
+				break;
+			}
+		}
+		assert(patternID != ID_FAIL);
+		DBGLOG(DBG, "patternID=" << patternID);
+		const OrdinaryAtom& pattern = ctx->registry()->ogatoms.getByAddress(patternID.address);
+
+		// for all related auxiliaries
+		BOOST_FOREACH (ID auxID, auxes){
+			DBGLOG(DBG, "Matching auxID=" << auxID << " (#parameters=" << inputType.size() << ")");
+			const OrdinaryAtom& aux = ctx->registry()->ogatoms.getByAddress(auxID.address);
+
+			// find a translation of the tuple of pattern to the tuple of aux
+			ID patternMazePred = pattern.tuple[1];
+			ID auxMazePred = aux.tuple[1];
+			int auxT = aux.tuple.size() < 6 ? -1 : aux.tuple[5];
+
+			// translate the nogood
+			Nogood translatedNG;
+			BOOST_FOREACH (ID lID, ng){
+				const OrdinaryAtom& l = ctx->registry()->ogatoms.getByAddress(lID.address);
+				if (lID != patternID){
+					OrdinaryAtom t = l;
+					if (t.tuple[0] = patternMazePred){
+						t.tuple[0] = auxMazePred;
+						if (t.tuple.size() == 5){
+							t.tuple[4] = ID::termFromInteger(auxT);
+						}
+						translatedNG.insert(NogoodContainer::createLiteral(ctx->registry()->storeOrdinaryGAtom(t).address, !lID.isNaf()));
+					}
+				}else{
+					OrdinaryAtom t = l;
+					t.tuple[1] = auxMazePred;
+					if (t.tuple.size() == 6){
+						t.tuple[5] = ID::termFromInteger(auxT);
+					}
+					translatedNG.insert(NogoodContainer::createLiteral(ctx->registry()->storeOrdinaryGAtom(t).address, !lID.isNaf()));
+				}
+			}
+
+			// store the translated nogood
+			DBGLOG(DBG, "Adding generalized nogood " << translatedNG.getStringRepresentation(ctx->registry()) << " (from " << ng.getStringRepresentation(ctx->registry()) << ")");
+			nogoods->addNogood(translatedNG);
+		}
+	}
 
 	virtual void
 	retrieve(const Query& query, Answer& answer, ProgramCtx* ctx, NogoodContainerPtr nogoods) throw (PluginError)
@@ -355,6 +488,71 @@ public:
 
 		// learning
 		if (nogoods){
+			if (query.pattern[0].isIntegerTerm() && query.pattern[1].isIntegerTerm()){
+				if (m.isReachable(query.pattern[0].address, query.pattern[1].address)){
+
+					// find a path
+					m.findpath(query.input[1].address, query.input[2].address, query.pattern[0].address, query.pattern[1].address);
+
+					// store it as reason in the nogood
+					Nogood reachableNG;
+					for (int x = 1; x <= m.getWidth(); ++x){
+						for (int y = 1; y <= m.getHeight(); ++y){
+							int ue = m.getUsedEdges(x, y);
+							if (ue & Maze::LEFT_MASK) reachableNG.insert(edgeToLiteral(query.input[0], x, y, Maze::LEFT_MASK, true));
+							if (ue & Maze::RIGHT_MASK) reachableNG.insert(edgeToLiteral(query.input[0], x, y, Maze::RIGHT_MASK, true));
+							if (ue & Maze::UP_MASK) reachableNG.insert(edgeToLiteral(query.input[0], x, y, Maze::UP_MASK, true));
+							if (ue & Maze::DOWN_MASK) reachableNG.insert(edgeToLiteral(query.input[0], x, y, Maze::DOWN_MASK, true));
+						}
+					}
+
+					Tuple t;
+					t.push_back(ID::termFromInteger(query.pattern[0].address));
+					t.push_back(ID::termFromInteger(query.pattern[1].address));
+					reachableNG.insert(NogoodContainer::createLiteral(getOutputAtom(ctx, nogoods, query, t, false)));
+					nogoods->addNogood(reachableNG);
+std::cout << "Reachable: " << reachableNG << std::endl;
+				}else{
+					// find the border between reachable and unreachable fields
+					Nogood unreachableNG;
+					for (int x = 1; x <= m.getWidth(); ++x){
+						for (int y = 1; y <= m.getHeight(); ++y){
+							if (m.isBorderField(x, y)){
+								int rx, ry, rl;
+
+								// find a reason why this field is a border field and encode it in the nogood
+								if (x > 1 && !m.isReachable(x - 1, y)){
+									m.getBorderReason(x, y, x - 1, y, &rx, &ry, &rl);
+									unreachableNG.insert(edgeToLiteral(query.input[0], rx, ry, rl, false));
+								}
+								if (x < m.getWidth() && !m.isReachable(x + 1, y)){
+									m.getBorderReason(x, y, x + 1, y, &rx, &ry, &rl);
+									unreachableNG.insert(edgeToLiteral(query.input[0], rx, ry, rl, false));
+								}
+								if (y > 1 && !m.isReachable(x, y - 1)){
+									m.getBorderReason(x, y, x, y - 1, &rx, &ry, &rl);
+									unreachableNG.insert(edgeToLiteral(query.input[0], rx, ry, rl, false));
+								}
+								if (y < m.getHeight() && !m.isReachable(x, y + 1)){
+									m.getBorderReason(x, y, x, y + 1, &rx, &ry, &rl);
+									unreachableNG.insert(edgeToLiteral(query.input[0], rx, ry, rl, false));
+								}
+							}
+						}
+					}
+
+					Tuple t;
+					t.push_back(ID::termFromInteger(query.pattern[0].address));
+					t.push_back(ID::termFromInteger(query.pattern[1].address));
+					unreachableNG.insert(NogoodContainer::createLiteral(getOutputAtom(ctx, nogoods, query, t, true)));
+					nogoods->addNogood(unreachableNG);
+std::cout << "Unreachable: " << unreachableNG << std::endl;
+//std::cout << unreachableNG.getStringRepresentation(reg) << std::endl;
+//exit(0);
+				}
+			}
+
+/*
 			// for all fields on the border between the reachable and the unreachable area
 			Nogood unreachableNG, reachableNG;
 			for (int x = 1; x <= m.getWidth(); ++x){
@@ -419,10 +617,7 @@ public:
 					}
 				}
 			}
-
-			// due to monotonicity, the existing connections together for a reason for the existence of a path to all reachable fields
-//			int s = nogoods->getNogoodCount();
-//			learnFromInputOutputBehavior(ctx, nogoods, query, this->prop, answer);
+*/
 		}
 	}
 };
