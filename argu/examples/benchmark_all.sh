c00b51e5be32bdcc89010bfb209a005a16325d48
Christoph Redl <redl@kr.tuwien.ac.at>
2012-11-19 11:58:45 +0100

    add benchmark script for better parallelization

--- argu/examples/benchmark_all.sh
+++ argu/examples/benchmark_all.sh
@@ -15,23 +15,23 @@ fi
 cd argubenchmark
 for instance in *.argu
 do
-#	echo "
-#		Executable = ../benchmark_single.sh
-#		Universe = vanilla
-#		output = $instance.out
-#		error = $instance.error
-#		Log = $instance.log
-#		Requirements = machine == "node3.kr.tuwien.ac.at"
-#		request_memory = 4096 
-#		Initialdir = /mnt/lion/home/redl/hexhex_dlvhex/mcsieplugin/examples/argubenchmark/
-#		notification = never
-#
-#		# queue
-#		request_cpus = 1 
-#		Arguments = $instance "$confstr" $to
-#		Queue 1
-#	     " > p.job
-#	condor_submit p.job
-	../benchmark_single.sh $instance "$confstr" $to
+	echo "
+		Executable = ../benchmark_single.sh
+		Universe = vanilla
+		output = $instance.out
+		error = $instance.error
+		Log = $instance.log
+		Requirements = machine == "node3.kr.tuwien.ac.at"
+		request_memory = 4096 
+		Initialdir = /mnt/lion/home/redl/hexhex_dlvhex/mcsieplugin/examples/argubenchmark/
+		notification = never
+
+		# queue
+		request_cpus = 1 
+		Arguments = $instance "$confstr" $to
+		Queue 1
+	     " > p.job
+	condor_submit p.job
+#	./../benchmark_single.sh $instance "$confstr" $to
 done
 
