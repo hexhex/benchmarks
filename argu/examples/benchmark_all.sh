d69b83bb96ec7642895a6ede094821a796986fa0
Christoph Redl <redl@kr.tuwien.ac.at>
2013-04-13 19:53:24 +0200

    update benchmark script

--- argu/examples/benchmark_all.sh
+++ argu/examples/benchmark_all.sh
@@ -15,9 +15,9 @@ do
 		output = $instance.out
 		error = $instance.error
 		Log = $instance.log
-		Requirements = machine == \"node3.kr.tuwien.ac.at\"
+		Requirements = machine == \"lion.kr.tuwien.ac.at\"
 		request_memory = 4096 
-		Initialdir = /mnt/lion/home/redl/hexhex_dlvhex/mcsieplugin/examples/argubenchmark/
+		Initialdir = $PWD
 		notification = never
 
 		# queue
