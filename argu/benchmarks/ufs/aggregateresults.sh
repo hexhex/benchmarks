a09c2ae2a4e690c1154804fe64b910af317bc291
Christoph Redl <redl@kr.tuwien.ac.at>
2013-05-04 13:29:12 +0200

    update statistics script

--- argu/benchmarks/ufs/aggregateresults.sh
+++ argu/benchmarks/ufs/aggregateresults.sh
@@ -45,21 +45,30 @@ mixed <- sapply(seq(1,ncol(merged)), FUN=g)
 merged <- merged[mixed]
 
 # round all values except in column 1
-output <-round(merged[,-1],2)
+output <- merged
+output[,-1] <- round(merged[,-1],2)
 
-write.table(format(output, digits=2), , , FALSE, , , , , TRUE, FALSE)
+write.table(format(output, digits=2), , , FALSE, , , , , FALSE, FALSE)
 "
 
 while read line
 do
 	read -a array <<< "$line"
 	fn=${array[0]}
-	array[0]="${fn:$extrstart:$extrlen} 1"
+	if [ $extrlen -ge 1 ]; then
+		array[0]="${fn:$extrstart:$extrlen} 1"
+	else
+		array[0]="${array[0]} 1"
+	fi
 	line=$(echo ${array[@]} | grep -v "#" | sed "s/\ \([0-9]*\)\.\([0-9]*\)/ \1.\2 0/g" | sed "s/---/$to 1/g")
 	file=$(echo "$file\n$line")
 done
 if [ $totex -ge 1 ]; then
-	echo -e $file | Rscript <(echo "$aggregate") | sed "s/\\n/\\\\\\n/g" | sed "s/\([0-9]*\.[0-9]*\)\ *\([0-9]*\)/\& \1 (\2)/g" | sed "s/$/ \\\\\\\\/g"
+	# 1. remove blanks at beginning of lines
+	# 2. add \\ to end of lines
+	# 3. " INT " --> " (INT) & "
+	# 4. " & \\" -> " \\"
+	echo -e $file | Rscript <(echo "$aggregate") | sed "s/^ *//g" | sed "s/$/ \\\\\\\\/g" | sed "s/ \([0-9][0-9]*\) / (\1) \& /g" | sed "s/\& *\\\\\\\\/\\\\\\\\/g"
 else
 	echo -e $file | Rscript <(echo "$aggregate")
 fi
