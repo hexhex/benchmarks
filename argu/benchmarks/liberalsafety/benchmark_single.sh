e4f606e640d0d046e44ebfa55f8715f3855e2418
Christoph Redl <redl@kr.tuwien.ac.at>
2013-05-07 20:43:04 +0200

    update benchmark script

--- argu/benchmarks/liberalsafety/benchmark_single.sh
+++ argu/benchmarks/liberalsafety/benchmark_single.sh
@@ -28,14 +28,14 @@ do
 	echo -ne -e " "
 	dir=$PWD
 	cd ../../../src
-	cmd="timeout $to time -o $dir/$instance.time.dat -f %e dlvhex2 $c --plugindir=.:../../../core/testsuite --argumode=idealset $dir/$instance $dir/../aggregate.hex --verbose=8"
-	output=$($cmd 2>$dir/$instance.verbose.dat >/dev/null)
+	cmd="timeout $to time -o $dir/$instance.$i.time.dat -f %e dlvhex2 $c --plugindir=.:../../../core/testsuite --argumode=idealset $dir/$instance $dir/../aggregate.hex --verbose=8"
+	output=$($cmd 2>$dir/$instance.$i.verbose.dat >/dev/null)
 	ret=$?
 
-        if [[ $ret == 0 ]]; then
-		output=$(cat $dir/$instance.time.dat)
-		groundertime=$(cat $dir/$instance.verbose.dat | grep -a "HEX grounder time:" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
-	        solvertime=$(cat $dir/$instance.verbose.dat | grep -a "HEX solver time:" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
+	if [[ $ret == 0 ]]; then
+		output=$(cat $dir/$instance.$i.time.dat)
+		groundertime=$(cat $dir/$instance.$i.verbose.dat | grep -a "HEX grounder time:" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
+		solvertime=$(cat $dir/$instance.$i.verbose.dat | grep -a "HEX solver time:" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
 	else
         	output="---"
   	        groundertime="---"
@@ -43,10 +43,10 @@ do
 	fi
 	echo -ne "$output $groundertime $solvertime"
 
+	rm $dir/$instance.$i.time.dat
+	rm $dir/$instance.$i.verbose.dat
+
 	cd $dir
 	let i=i+1
 done
 echo -e -ne "\n"
-
-rm $dir/$instance.time.dat
-rm $dir/$instance.verbose.dat
