3d131655ec37d38f3f92bf7a3c59a161fd5e34d8
Christoph Redl <redl@kr.tuwien.ac.at>
2013-10-20 22:34:37 +0200

    add benchmark scripts

---
+++ maze/examples/benchmark_single.sh
@@ -0,0 +1,49 @@
+# $1: instance
+# $2: timeout
+export PATH=$1
+export LD_LIBRARY_PATH=$2
+instance=$3
+to=$4
+
+confstr=" "
+confstr2=$(cat ../conf)
+if [ $? == 0 ]; then
+        confstr=$confstr2
+fi
+
+# split configurations
+IFS=';' read -ra confs <<< "$confstr"
+header="#size"
+i=0
+for c in "${confs[@]}"
+do
+	header="$header   \"$c\""
+	let i=i+1
+done
+echo $header
+
+# do benchmark
+echo -ne "$instance"
+
+# for all configurations
+i=0
+for c in "${confs[@]}"
+do
+	echo -ne -e " "
+	dir=$PWD
+	cd ../../src
+	cmd="timeout $to time -o $dir/$instance.time.dat -f %e dlvhex2 $c --plugindir=. --extlearn --evalall --liberalsafety $dir/$instance"
+
+	$($cmd 2>/dev/null >/dev/null)
+	ret=$?
+	output=$(cat $dir/$instance.time.dat)
+	if [[ $ret == 124 ]]; then
+		output="---"
+	fi
+	cd $dir
+	echo -ne $output
+	rm $dir/$instance.time.dat
+	let i=i+1
+done
+echo -e -ne "\n"
+
