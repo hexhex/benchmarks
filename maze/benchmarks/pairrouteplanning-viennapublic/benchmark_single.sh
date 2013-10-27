3845b1138b59d7790048ea61d046a29669971329
Christoph Redl <redl@kr.tuwien.ac.at>
2013-10-27 21:06:53 +0100

    change heuristics

--- maze/benchmarks/pairrouteplanning-viennapublic/benchmark_single.sh
+++ maze/benchmarks/pairrouteplanning-viennapublic/benchmark_single.sh
@@ -32,7 +32,7 @@ do
 	echo -ne -e " "
 	dir=$PWD
 	cd ..
-	cmd="timeout $to time -o $dir/$instance.$i.time.dat -f %e dlvhex2 $c --plugindir=../../src --extlearn --evalall -n=1 $dir/$instance --heuristics=easy --verbose=8"
+	cmd="timeout $to time -o $dir/$instance.$i.time.dat -f %e dlvhex2 $c --plugindir=../../src --extlearn --evalall -n=1 $dir/$instance --verbose=8"
 	$($cmd 2>$dir/$instance.$i.verbose.dat >/dev/null)
 	ret=$?
 	cd instances
