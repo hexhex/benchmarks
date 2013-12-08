9b7541344d5a8a60cc672d5eea330f9693c7f12f
Christoph Redl <redl@kr.tuwien.ac.at>
2013-12-08 16:39:51 +0100

    update benchmark configuration

--- sensor/benchmarks/benchmark_single.sh
+++ sensor/benchmarks/benchmark_single.sh
@@ -7,7 +7,7 @@ export LD_LIBRARY_PATH=$2
 instance=$3
 to=$4
 
-confstr="--flpcheck=explicit --noflpcriterion;--flpcheck=explicit --noflpcriterion --extlearn;--flpcheck=ufsm --noflpcriterion;--flpcheck=ufsm --extlearn --noflpcriterion;--flpcheck=ufsm --extlearn --ufslearn --noflpcriterion;--flpcheck=ufs;--flpcheck=ufs --extlearn;--flpcheck=ufs --extlearn --ufslearn;--flpcheck=aufs;--flpcheck=aufs --extlearn;--flpcheck=aufs --extlearn --ufslearn;--flpcheck=explicit --noflpcriterion -n=1;--flpcheck=explicit --noflpcriterion --extlearn -n=1;--flpcheck=ufsm -n=1;--flpcheck=ufsm --extlearn --noflpcriterion -n=1;--flpcheck=ufsm --extlearn --ufslearn --noflpcriterion -n=1;--flpcheck=ufs -n=1;--flpcheck=ufs --extlearn -n=1;--flpcheck=ufs --extlearn --ufslearn -n=1;--flpcheck=aufs -n=1;--flpcheck=aufs --extlearn -n=1;--flpcheck=aufs --extlearn --ufslearn -n=1"
+confstr="--flpcheck=explicit --noflpcriterion;--flpcheck=explicit --extlearn --noflpcriterion;--flpcheck=ufsm --noflpcriterion;--flpcheck=ufsm --extlearn --noflpcriterion;--flpcheck=ufsm --extlearn --ufslearn --noflpcriterion;--flpcheck=ufs;--flpcheck=ufs --extlearn;--flpcheck=ufs --extlearn --ufslearn;--flpcheck=aufs;--flpcheck=aufs --extlearn;--flpcheck=aufs --extlearn --ufslearn;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=periodic;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=max;--flpcheck=explicit --noflpcriterion -n=1;--flpcheck=explicit --noflpcriterion --extlearn -n=1;--flpcheck=ufsm -n=1;--flpcheck=ufsm --extlearn --noflpcriterion -n=1;--flpcheck=ufsm --extlearn --ufslearn --noflpcriterion -n=1;--flpcheck=ufs -n=1;--flpcheck=ufs --extlearn -n=1;--flpcheck=ufs --extlearn --ufslearn -n=1;--flpcheck=aufs -n=1;--flpcheck=aufs --extlearn -n=1;--flpcheck=aufs --extlearn --ufslearn -n=1;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=periodic -n=1;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=max -n=1"
 
 # split configurations
 IFS=';' read -ra confs <<< "$confstr"
@@ -28,7 +28,7 @@ i=0
 for c in "${confs[@]}"
 do
 	echo -ne -e " "
-	$(timeout $to time --quiet -o $instance.time.dat -f %e dlvhex2 $c --plugindir=../../src ../domain_anyobjectposition.hex $instance 2>/dev/null >/dev/null)
+	$(timeout $to time --quiet -o $instance.time.dat -f %e dlvhex2 $c --claspconfig=none --plugindir=../../src ../domain_anyobjectposition.hex $instance 2>/dev/null >/dev/null)
 	ret=$?
 	output=$(cat $instance.time.dat)
 	if [[ $ret != 0 ]]; then
--- sensor/src/sensors.cpp
+++ sensor/src/sensors.cpp
@@ -190,109 +190,6 @@ bool extractTimeLoc(TimeLocMap& tlm, IDSet& relevantIDs, RegistryPtr reg, Interp
 	return true;
 }
 
-bool extractTimeLocOneObj(TimeLocMap& tlm, IDSet& relevantIDs, RegistryPtr reg, InterpretationConstPtr pi,
-		const ID robotxpr, const ID robotypr, const ID objxpr, const ID objypr)
-{
-	// find relevant input
-	bm::bvector<>::enumerator en = pi->getStorage().first();
-	bm::bvector<>::enumerator en_end = pi->getStorage().end();
-	while (en < en_end)
-	{
-		// id of current true bit
-		ID id(ID::MAINKIND_ATOM | ID::SUBKIND_ATOM_ORDINARYG, *en);
-		const OrdinaryAtom& atom = reg->ogatoms.getByID(id);
-		if( atom.tuple.size() == 2 )
-		{
-			// {objectX,objectY}(Coordinate)
-			ID pred = atom.tuple[0];
-			ID entity;
-			ID coordinate = atom.tuple[1];
-			LOG(INFO,"got tuple " << printToString<RawPrinter>(id,reg));
-
-			// refer -> if not exists, create empty ones
-			LocMap &objLoc = tlm.dobjects;
-			if( pred == objxpr || pred == objypr ) {
-				// either robot x or y coordinate
-				if( pred == objxpr ) {
-					ID& objX = objLoc[entity].first;
-					if( objX != ID_FAIL )
-						// duplicate assignment -> abort
-						return false;
-					else
-						objX = coordinate;
-				}
-				else {
-					ID& objY = objLoc[entity].second;
-					if( objY != ID_FAIL )
-						// duplicate assignment -> abort
-						return false;
-					else
-						objY = coordinate;
-				}
-				relevantIDs.insert(id);
-			}
-		}
-		else if( atom.tuple.size() == 4 )
-		{
-			// {robotX,robotY,objectX,objectY}(Robot,Coordinate,Step)
-			ID pred = atom.tuple[0];
-			ID entity = atom.tuple[1];
-			ID coordinate = atom.tuple[2];
-			ID step = atom.tuple[3];
-			LOG(INFO,"got tuple " << printToString<RawPrinter>(id,reg));
-
-			if( pred == robotxpr || pred == robotypr ) {
-				// either robot x or y coordinate
-
-				// refer -> if not exists, create empty ones
-				LocMap &roboLoc = tlm.robots[step];
-				if( pred == robotxpr ) {
-					ID& roboX = roboLoc[entity].first;
-					if( roboX != ID_FAIL )
-						// duplicate assignment -> abort
-						return false;
-					else
-						roboX = coordinate;
-				}
-				else {
-					ID& roboY = roboLoc[entity].second;
-					if( roboY != ID_FAIL )
-						// duplicate assignment -> abort
-						return false;
-					else
-						roboY = coordinate;
-				}
-				relevantIDs.insert(id);
-			}
-			else if( pred == objxpr || pred == objypr ) {
-				// either object x or y coordinate
-
-				// refer -> if not exists, create empty ones
-				LocMap &objLoc = tlm.objects[step];
-				if( pred == objxpr ) {
-					ID& objX = objLoc[entity].first;
-					if( objX != ID_FAIL )
-						// duplicate assignment -> abort
-						return false;
-					else
-						objX = coordinate;
-				}
-				else {
-					ID& objY = objLoc[entity].second;
-					if( objY != ID_FAIL )
-						// duplicate assignment -> abort
-						return false;
-					else
-						objY = coordinate;
-				}
-				relevantIDs.insert(id);
-			}
-		}
-		en++;
-	}
-	return true;
-}
-
 // return false if any coordinate is only partially specified (i.e., ID_FAIL for one component)
 bool checkTimeLocMap(const TimeLocMap& tlm) {
 	// collect maps to check
@@ -343,7 +240,7 @@ public:
 		addInputPredicate();
 		// object (all objects we must spoil for)
 		addInputPredicate();
-
+		
 		// true for all objects found in range
 		setOutputArity(1);
 	}
@@ -451,136 +348,6 @@ public:
 				out.push_back(obj);
 				answer.get().push_back(out);
 			}
-		}
-		while(false);
-		answer.use();
-	}
-
-	/*
-	throw PluginError("method should not be called, we use extlearning!");
-	virtual void
-	retrieve(const Query& query, Answer& answer, NogoodContainerPtr nogoods) throw (PluginError)
-	{
-	}
-	*/
-};
-
-class SenseOneObjectAtom: public PluginAtom, public SensorRangeCheck
-{
-public:
-	SenseOneObjectAtom():
-		PluginAtom("senseOneObject", false)
-	{
-		// robotX(Robot,X,Step)
-		addInputPredicate();
-		// robotY(Robot,Y,Step)
-		addInputPredicate();
-		// objectX(X)
-		addInputPredicate();
-		// objectY(Y)
-		addInputPredicate();
-		// RangeString
-		addInputConstant();
-		// spoil (just be true if this is true)
-		addInputPredicate();
-
-		// true for all objects found in range
-		setOutputArity(0);
-	}
-
-	void collectFoundObjects(IDSet& foundObjects, const TimeLocMap& tlm, float range)
-	{
-		const LocMap& olm = tlm.dobjects;
-		
-		for(std::map<ID, LocMap>::const_iterator itt = tlm.robots.begin(); itt != tlm.robots.end(); ++itt)
-		{
-			LOG(DBG,"collecting found objects for time step " << itt->first.address);
-			const LocMap& rlm = itt->second;
-			// for all robots
-			for(LocMap::const_iterator rit = rlm.begin(); rit != rlm.end(); ++rit)
-			{
-				LOG(DBG,"  checking robot " << rit->first);
-				// for all objects
-				for(LocMap::const_iterator oit = olm.begin(); oit != olm.end(); ++oit)
-				{
-					LOG(DBG,"  checking object " << oit->first);
-
-					ID robot = rit->first;
-					unsigned rx = rit->second.first.address;
-					unsigned ry = rit->second.second.address;
-					ID object = oit->first;
-					unsigned ox = oit->second.first.address;
-					unsigned oy = oit->second.second.address;
-					if( canSee(range, rx, ry, ox, oy) )
-						foundObjects.insert(object);
-				}
-			}
-
-		}
-	}
-
-	virtual void retrieve(const Query& query, Answer& answer) throw (PluginError)
-	{
-		DLVHEX_BENCHMARK_REGISTER_AND_SCOPE(sid,"senseOneObject");
-
-		RegistryPtr reg = getRegistry();
-
-		InterpretationConstPtr pi = query.interpretation;
-
-		LOG(INFO,"senseOneObject! " << *pi);
-
-		const ID robotxpr = query.input[0];
-		const ID robotypr = query.input[1];
-		const ID objxpr = query.input[2];
-		const ID objypr = query.input[3];
-		const ID sensorrange = query.input[4];
-		const ID spoilterm = query.input[5];
-
-		OrdinaryAtom spoilatom(ID::MAINKIND_ATOM | ID::SUBKIND_ATOM_ORDINARYG);
-		spoilatom.tuple.push_back(spoilterm);
-		const ID spoilpred = reg->storeOrdinaryGAtom(spoilatom);
-		if( pi->getFact(spoilpred.address) ) {
-			// spoiled -> true for all objects
-			answer.get().push_back(Tuple());
-			LOG(INFO,"spoil active -> return true");
-			return;
-		}
-
-		IDSet relevantIDs;
-
-		TimeLocMap tlm;
-		bool aborted = true;
-		do {
-			// partial
-			if( !extractTimeLocOneObj(tlm, relevantIDs, reg, pi, robotxpr, robotypr, objxpr, objypr) ) {
-				LOG(INFO,"extractTimeLoc aborted");
-				break;
-			}
-
-			// partial
-			if( !checkTimeLocMap(tlm) ) {
-				LOG(INFO,"checkTimeLocMap aborted");
-				break;
-			}
-
-			const std::string& sensor = reg->getTermStringByID(sensorrange);
-			float range;
-			{
-				std::stringstream ss;
-				if( sensor[0] == '"' )
-					ss << sensor.substr(1,sensor.size()-2);
-				else
-					ss << sensor;
-				ss >> range;
-			}
-			LOG(INFO,"checking with sensor range " << range);
-
-			IDSet foundObjects;
-			collectFoundObjects(foundObjects, tlm, range);
-
-			aborted = false;
-			if( !foundObjects.empty() )
-				answer.get().push_back(Tuple());
 		}
 		while(false);
 		answer.use();
@@ -756,12 +523,11 @@ class SensorsPlugin : public PluginInterface
 		virtual std::vector<PluginAtomPtr> createAtoms(ProgramCtx& ctx) const
 		{
 			std::vector<PluginAtomPtr> ret;
-
-			ctx.benchmarksToSnapshotAtFirstModel.insert(std::make_pair("senseObject", "senseObject calls to first model"));
+			
+//			ctx.benchmarksToSnapshotAtFirstModel.insert(std::make_pair("senseObject", "senseObject calls to first model"));
 
 			// return smart pointer with deleter (i.e., delete code compiled into this plugin)
 			ret.push_back(PluginAtomPtr(new SenseObjectAtom, PluginPtrDeleter<PluginAtom>()));
-			ret.push_back(PluginAtomPtr(new SenseOneObjectAtom, PluginPtrDeleter<PluginAtom>()));
 			ret.push_back(PluginAtomPtr(new SenseObjectVLAtom, PluginPtrDeleter<PluginAtom>()));
 
 			return ret;
