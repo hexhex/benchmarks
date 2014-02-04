4d3787affdb875c58bca52cafbe393d5330aa874
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-04 17:35:11 +0100

    update merging script

--- scripts/mergetables.sh
+++ scripts/mergetables.sh
@@ -7,6 +7,10 @@ fi
 cmd="paste -d ' '"
 for (( i=1; i<=$#; i++ ))
 do
-	cmd="$cmd <(cat ${!i} | sed 's/^ *//g' | sed 's/ \+/ /g' | cut -d\" \" -f3-)"
+	if [[ $i -ge 2 ]]; then
+		cmd="$cmd <(cat ${!i} | sed 's/^ *//g' | sed 's/ \+/ /g' | cut -d\" \" -f3-)"
+	else
+                cmd="$cmd <(cat ${!i} | sed 's/^ *//g' | sed 's/ \+/ /g' | cut -d\" \" -f1-)"
+	fi
 done
 eval $cmd
