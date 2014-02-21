7dee438e608d554cca5899a96d4a8f2c39aa94ad
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-21 19:47:39 +0100

    update benchmark scripts

--- scripts/aggregateresults.sh
+++ scripts/aggregateresults.sh
@@ -17,8 +17,8 @@ else
 fi
 
 if [ $# -le 3 ]; then
-	meanCols="c(1, seq(3, ncol(input), 2))"
-	sumCols="c(1, 2, seq(4, ncol(input), 2))"
+	meanCols="if (ncol(input) >= 3) c(1, seq(3, ncol(input), 2)) else c(1)"
+	sumCols="if (ncol(input) >= 2) c(1, seq(2, ncol(input), 2)) else c(1)"
 	maxCols="c(1)"
 	minCols="c(1)"
 else
@@ -60,67 +60,67 @@ if [ $error -eq 1 ]; then
 fi
 
 aggregate="
-library(doBy);
+	library(doBy);
 
-input <- read.table('stdin', header=FALSE, as.is=TRUE)
+	input <- read.table('stdin', header=FALSE, as.is=TRUE)
 
-# extract odd and even columns
-meanCols <- unique($meanCols)
-sumCols <- unique($sumCols)
-maxCols <- unique($maxCols)
-minCols <- unique($minCols)
+	# extract odd and even columns
+	meanCols <- unique($meanCols)
+	sumCols <- unique($sumCols)
+	maxCols <- unique($maxCols)
+	minCols <- unique($minCols)
 
-# make a copy of the table for each aggregation function,
-# where all columns not relevant to this function are 0
-emptytab <- input
-emptytab[, ] <- 0
-meanInput <- emptytab
-maxInput <- emptytab
-minInput <- emptytab
-sumInput <- emptytab
-meanInput[, meanCols] <- input[, meanCols]
-sumInput[, sumCols] <- input[, sumCols]
-maxInput[, maxCols] <- input[, maxCols]
-minInput[, minCols] <- input[, minCols]
+	# make a copy of the table for each aggregation function,
+	# where all columns not relevant to this function are 0
+	emptytab <- input
+	emptytab[, ] <- 0
+	meanInput <- emptytab
+	maxInput <- emptytab
+	minInput <- emptytab
+	sumInput <- emptytab
+	meanInput[, meanCols] <- input[, meanCols]
+	sumInput[, sumCols] <- input[, sumCols]
+	maxInput[, maxCols] <- input[, maxCols]
+	minInput[, minCols] <- input[, minCols]
 
-inputcomp <- input
-inputcomp[, 1] <- input[, 1] * 4
-if ( !all( (meanInput + sumInput + maxInput + minInput) == inputcomp) ){
-	print(\"Error: There must be exactly one aggregation method specified for each column\");
-	return(1)
-}
+	inputcomp <- input
+	inputcomp[, 1] <- input[, 1] * 4
+	if ( !all( (meanInput + sumInput + maxInput + minInput) == inputcomp) ){
+		print(\"Error: There must be exactly one aggregation method specified for each column\");
+		return(1)
+	}
 
-# compute the aggregation functions
-output <- emptytab
-meanResult <- summaryBy(.~V1, data=meanInput, FUN=mean)
-sumResult <- summaryBy(.~V1, data=sumInput, FUN=sum)
-maxResult <- summaryBy(.~V1, data=maxInput, FUN=max)
-minResult <- summaryBy(.~V1, data=minInput, FUN=min)
+	# compute the aggregation functions
+	output <- emptytab
+	meanResult <- summaryBy(.~V1, data=meanInput, FUN=mean)
+	sumResult <- summaryBy(.~V1, data=sumInput, FUN=sum)
+	maxResult <- summaryBy(.~V1, data=maxInput, FUN=max)
+	minResult <- summaryBy(.~V1, data=minInput, FUN=min)
 
-# assemble the final result
-output <- meanResult + maxResult + minResult + sumResult
-output[, 1] <- meanResult[, 1]
+	# assemble the final result
+	output <- meanResult + maxResult + minResult + sumResult
+	output[, 1] <- meanResult[, 1]
 
-# compute for each column if it is an integer column
-outCols <- seq(1, ncol(output))
-isIntCol <- sapply(outCols, function(x) { return(all(round(output[, x], 0)==output[, x])) } )
-isNonIntCol <- sapply(outCols, function(x) { return(all(round(output[, x], 0)!=output[, x])) } )
+	# compute for each column if it is an integer column
+	outCols <- seq(1, ncol(output))
+	isIntCol <- sapply(outCols, function(x) { return(all(round(output[, x], 0)==output[, x])) } )
+	isNonIntCol <- sapply(outCols, function(x) { return(all(round(output[, x], 0)!=output[, x])) } )
 
-# rounding and output formatting
-outputformatted <- output
+	# rounding and output formatting
+	outputformatted <- output
 
-if (!all(isNonIntCol)){
-	intCols <- outCols[isIntCol[outCols]]
-	outputformatted[, intCols] <- format(output[, intCols], nsmall=0)
-}
-if (!all(isIntCol)){
-	nonIntCols <- outCols[isNonIntCol[outCols]]
-	outputformatted[, nonIntCols] <- format(round(output[, nonIntCols], 2), nsmall=2)
-}
+	if (!all(isNonIntCol)){
+		intCols <- outCols[isIntCol[outCols]]
+		outputformatted[, intCols] <- format(output[, intCols], nsmall=0)
+	}
+	if (!all(isIntCol)){
+		nonIntCols <- outCols[isNonIntCol[outCols]]
+		outputformatted[, nonIntCols] <- format(round(output[, nonIntCols], 2), nsmall=2)
+	}
 
-# output
-write.table(outputformatted, , , FALSE, , , , , FALSE, FALSE)
-"
+	# output
+	write.table(outputformatted, , , FALSE, , , , , FALSE, FALSE)
+	"
 
 # add a trailing # in order to eliminate the last line if incomplete
 data=$(mktemp)
@@ -149,9 +149,9 @@ do
 		fi
 
 		if [ $extrlen -ge 1 ]; then
-			array[0]="${fn:$extrstart:$extrlen} 1"
+			array[0]="${fn:$extrstart:$extrlen}"
 		else
-			array[0]="${array[0]} 1"
+			array[0]="${array[0]}"
 		fi
 	        line=$(echo ${array[@]} | grep -v "#")
 		file=$(echo "$file\n$line")
@@ -169,7 +169,7 @@ if [ $unknownvalues -gt 0 ]; then
 fi
 
 rerrfile=$(mktemp)
-echo -e $file | Rscript <(echo "$aggregate" | sed 's/\?\?\?/0/g') 2>$rerrfile
+echo -e "$file" | Rscript <(echo "$aggregate" | sed 's/???/0/g') 2>$rerrfile
 if [[ $? -ne 0 ]]; then
 	echo "Aggregation failed, R yielded the following error:" >&2
 	cat $rerrfile
