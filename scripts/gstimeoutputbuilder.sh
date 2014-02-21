22783a01da5871a9af6a501fd9a47798857d096e
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-21 23:07:03 +0100

    some fixes in the benchmark scripts

--- scripts/gstimeoutputbuilder.sh
+++ scripts/gstimeoutputbuilder.sh
@@ -16,8 +16,9 @@ fi
 
 ret=$1
 timefile=$2
-stdout=$3
-sterr=$4
+instout=$3
+insterr=$4
+
 if [[ $ret == 124 ]]; then
 	echo -ne "--- 1 --- 0 --- 0"
 	exit 0
@@ -29,22 +30,23 @@ else
 	time=$(cat $timefile)
 
 	# check if there is a grounding and solving time (should be the case for successful instances)
-	groundertime=$(cat $sterr | grep -a "HEX grounder time:")
-	solvertime=$(cat $sterr | grep -a "HEX solver time:")
-	haveGroundertime=$(echo -ne $groundertime | wc -l)
-	haveSolvertime=$(echo -ne $solvertime | wc -l)
+	groundertime=$(cat $insterr | grep -a "HEX grounder time:")
+	solvertime=$(cat $insterr | grep -a "HEX solver time:")
+	haveGroundertime=$(echo $groundertime | wc -l)
+	haveSolvertime=$(echo $solvertime | wc -l)
+
 	if [[ $haveGroundertime -eq 0 ]] || [[ $haveSolvertime -eq 0 ]]; then
 		echo "Instance did not provide grounder and solver time" >&2
 		groundertime="???"
 		solvertime="???"
 	else
 		# extract grounding and solving time
-		groundertime=$(echo $groundertime | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
-		solvertime=$(echo $solvertime | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
+		groundertime=$(echo "$groundertime" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
+		solvertime=$(echo "$solvertime" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
 
 		# round to two digits
-		groundertime=$(echo "scale=2; $groundertime/1" | bc)
-		solvertime=$(echo "scale=2; $solvertime/1" | bc)
+		groundertime=$(echo "scale=2; $groundertime/1" | bc | sed 's/^\./0\./')
+		solvertime=$(echo "scale=2; $solvertime/1" | bc | sed 's/^\./0\./')
 	fi
 
 	echo -ne "$time 0 $groundertime 0 $solvertime 0"
