d0653c93d2a3b47dfb5ae63c7b6f9b13c1c267b2
Christoph Redl <redl@kr.tuwien.ac.at>
2014-04-01 18:02:07 +0200

    update aggregation script

--- maze/benchmarks/pairrouteplanningnonrec-viennapublic/aggregate.sh
+++ maze/benchmarks/pairrouteplanningnonrec-viennapublic/aggregate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Call the default aggregation script as follows:
-#    aggregate 300 0 0 "3,5,6" "2,4,7,8,9" "" ""
-# Columns 3,5,6 will be averaged (contain the total runtime, grounding and solving time)
-# Columns 2,4,7,8,9 are added (2: number of instances, 4: timeout, 7,8,9: pathlen, changes, restaurant)
+#    aggregateresults.sh 300 0 0 "3,5,6,7,8,9,10,11,13,14,15,16,17,18,19,21,22,23,24,25,26" "2,4,12,20" "" ""
+# Columns 3,5,6,7,8,9,10,11,13,14,15,16,17,18,19,21,22,23,24,25,26 will be averaged (3,11,19 contain the total runtime, 5,13,22 grounding and 6,14,23 solving time)
+# Columns 2,4,12,20 are added (2: number of instances, 4,12,20: timeouts)
 # (Last two parameters say that we don't compute minimum or maximum of any columns)
-aggregateresults.sh 300 0 0 "3,5,6" "2,4,7,8,9" "" ""
+aggregateresults.sh 300 0 0 "3,5,6,7,8,9,10,11,13,14,15,16,17,18,19,21,22,23,24,25,26" "2,4,12,20" "" ""
