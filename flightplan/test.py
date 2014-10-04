1f36b005d5d7d73276d3bf54a28737108c72e1fe
Christoph Redl <redl@kr.tuwien.ac.at>
2014-10-05 00:28:28 +0200

    add new benchmark

---
+++ flightplan/test.py
@@ -0,0 +1,38 @@
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from datetime import datetime
+
+def getPrice(query):
+
+	q = ""
+	for stop in query:
+		q = q + "/" + stop[0] + "-" + stop[1] + "/" + stop[2].strftime('%Y-%m-%d')
+
+	# Start the WebDriver and load the page
+	wd = webdriver.Firefox()
+	wd.get("http://www.kayak.de/flights" + q)
+	
+	# Wait for the dynamically loaded elements to show up
+	
+	# And grab the page HTML source
+	html_page = wd.page_source
+	wd.quit()
+	
+	pattern = re.compile(u"bestPrice[^>]*>[0-9]+")
+	minprice = -1
+	for p in pattern.findall(html_page):
+		price = int(p[p.index('>') + 1:])
+		if minprice == -1 or price < minprice:
+			minprice = price
+	
+	return minprice
+
+print(getPrice( (
+					("VIE", "TXL", datetime(2015, 2, 3)),
+					("TXL", "NYC", datetime(2015, 2, 7)),
+					("NYC", "VIE", datetime(2015, 2, 10)),
+				) ))
+
